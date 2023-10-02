64c59c3a82fdbb7a37f255c26bdedaefe2edf6e3
Ryan Huie <ryanhuie@Ryans-MacBook-Air.local>
2023-10-02 16:43:15 -0700

    switched to react + flask stack

---
+++ server/server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+#home
+@app.route("/")
+def members():
+    return {"members": ["Member 1", "Member 2", "Member 6"]}
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
